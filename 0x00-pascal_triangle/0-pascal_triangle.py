505696efbf61a273aab1672162e4eddd67fe41ce
Alien <alongejoshua@gmail.com>
2023-04-08 23:24:51 +0100

    Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
 
-def pascal_triangle(number):
+def pascal_triangle(n):
     ''' 
         returns a list of lists of integers representing
         the Pascal's triangle of n
     '''
-    if (number <= 0):
-        return []
-    if (number == 1):
-        return [1]
-    lst = []
-    for outer in range(number):
-        if (outer == 0):
-            lst.append([1])
-        if (outer == 1):
-            lst.append([1,1])
-        tlst = []
-        temp1 = outer
-        temp2 = outer
-        for x in range(0, temp1+1):
-            y = x
-            if (x == 0 or x == outer):
-                tlst.append(1)
-            else:
-                tlst.append((lst[temp2-1][y-1]) + (lst[temp2-1][y+1]))
-        lst.append(tlst)         
+    triangle = []
+    for i in range(n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        if i > 0:
+            row.append(1)
+        triangle.append(row)
+    return triangle
+
 
 
