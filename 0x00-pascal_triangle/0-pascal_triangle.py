cc7aee8c5a73f6008a31810cb54e70208c8d6ab6
Alien <alongejoshua@gmail.com>
2023-04-08 23:29:20 +0100

    adjust code to PEP8 standard

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ This is the Pascal triangle """
 
+
 def pascal_triangle(n):
-    ''' 
+    '''
         returns a list of lists of integers representing
         the Pascal's triangle of n
     '''
@@ -15,6 +16,3 @@ def pascal_triangle(n):
             row.append(1)
         triangle.append(row)
     return triangle
-
-
-
